4c1e02266453ccbccf6fb11ebbb197d6fe2e903b
Yann Thibodeau <y.thibodeau1@gmail.com>
2020-12-08 10:33:35 -0500

    Added presentation on Redux

--- src/components/TextBlock.jsx
+++ src/components/TextBlock.jsx
@@ -1,10 +1,11 @@
 import React from "react";
 
-export const TextBlock = ({ children, margin }) => (
+export const TextBlock = ({ children, margin, style }) => (
   <div
     style={{
       margin: `${margin || 5}vw`,
       whiteSpace: "break-spaces",
+        ...style,
     }}
   >
     {children}
